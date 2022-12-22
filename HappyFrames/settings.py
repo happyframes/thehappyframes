7ac096505099deb791cb11836d76fe8166858179
happy_frames <happyframes.dev@gmail.com>
2022-12-22 22:45:13 +0530

    allowed hosts all

--- HappyFrames/settings.py
+++ HappyFrames/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thehappyframes.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
